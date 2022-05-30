8c5d06b75f3501cfcebd5e1f633792bc17b2dfec
Coossy <charles-emile.cossette@polymtl.ca>
2022-05-30 16:24:22 -0400

    yoyoyo

--- exercice.py
+++ exercice.py
@@ -3,7 +3,8 @@
 
 # TODO: Importez vos modules ici
 import math
-
+from turtle import * 
+color('green') 
 
 # TODO: Définissez vos fonction ici
 def ellipsoïde(a = 1, b = 2, c = 3, masse_volumique = 4): # m = p * V
@@ -21,12 +22,22 @@ def dictionnaire_lambda(phrase = "bonjour, je suis une phrase. je suis compose d
     return (sorted(dictionnaire, key=dictionnaire.__getitem__, reverse = True))
 
 
+def arbre_recursif(n = 4):
+    if n>0:
+        pensize(5)
+        forward(100)
+        left(45)
+        speed(1)
+        arbre_recursif(n-1)
+
+
+
+
+
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
     #print(ellipsoïde())
-
     #print(dictionnaire_lambda())
-    
-    
+    print(arbre_recursif())
     
     pass
